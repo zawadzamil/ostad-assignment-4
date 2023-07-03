e53a6acbf99d3c2cc0547672f8924e9e1877b8cb
zawadzamil <contact.zawadzamil@gmail.com>
2023-07-03 17:44:57 +0600

    product added

---
+++ assets/js/app.js
@@ -0,0 +1,1 @@
+console.log("---------products--------", products[0]);
---
+++ assets/js/products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+        id: 1,
+        name: "Vans Sk8-Hi MTE Shoes",
+        price: 10.99,
+        description: "The Vans All-Weather MTE Collection",
+        image: "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80",
+    },
+    {
+        id: 2,
+        name: "Adidas Superstar",
+        price: 99.99,
+        description: "Classic shell-toe sneakers",
+        image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+        id: 3,
+        name: "Nike Air Force 1",
+        price: 120.0,
+        description: "Iconic basketball shoes",
+        image: "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+        id: 4,
+        name: "Converse Chuck 70",
+        price: 80.0,
+        description: "Premium canvas sneakers",
+        image: "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+        id: 5,
+        name: "New Balance Fresh Foam 1080v11",
+        price: 149.99,
+        description: "Cushioned running shoes",
+        image: "https://images.unsplash.com/photo-1505233676147-accd9c73eb1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1362&q=80",
+    },
+    {
+        id: 6,
+        name: "Reebok Classic Leather",
+        price: 79.99,
+        description: "Timeless leather sneakers",
+        image: "https://images.unsplash.com/photo-1494496195158-c3becb4f2475?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+        id: 7,
+        name: "Puma Suede Classic",
+        price: 69.99,
+        description: "Iconic suede sneakers",
+        image: "https://images.unsplash.com/photo-1518527399940-f3f768f47dd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80",
+    },
+    {
+        id: 8,
+        name: "ASICS Gel-Kayano 27",
+        price: 159.99,
+        description: "Stability running shoes",
+        image: "https://images.unsplash.com/photo-1530389912609-9a007b3c38a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+        id: 9,
+        name: "Vans Old Skool",
+        price: 59.99,
+        description: "Classic skate shoes",
+        image: "https://images.unsplash.com/photo-1548100721-15f0e76035c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+        id: 10,
+        name: "Salomon Speedcross 5",
+        price: 139.99,
+        description: "Trail running shoes",
+        image: "https://images.unsplash.com/photo-1643584548984-7ecc5adeabf6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    },
+
+    // Add more products as needed
+];
