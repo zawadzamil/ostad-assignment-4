3e54e1dc1169d71f8bf811149fcab5a27cea3194
zawadzamil <contact.zawadzamil@gmail.com>
2023-07-04 23:02:24 +0600

    cart plus minus added

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -4,6 +4,7 @@ let cartArray = [];
 let cartPrice = 0;
 let cartDiscount = 0;
 let cartTotal = 0;
+let isAppliedCode = false;
 
 const addToCart = (productId) => {
     const product = products.find((product) => product.id == productId);
@@ -12,11 +13,170 @@ const addToCart = (productId) => {
         name: product.name,
         description: product.description,
         price: product.price,
+        image: product.image,
     };
 
     cartArray.push(newCart);
-    console.log(cartArray);
+    addCartElement(product);
+};
 
+function updatePrice(productId) {
+    const product = products.find((p) => p.id == productId);
+    cartPrice += product.price;
+    cartTotal += product.price;
+
+    document.getElementById("cart_price").innerText = cartPrice.toFixed(2);
+    document.getElementById("cart_total").innerText = cartTotal.toFixed(2);
+}
+function updateCartPrice() {
+    cartPrice = 0;
+    cartTotal = 0;
+    cartArray.forEach((cartItem) => {
+        const product = products.find((item) => item.id === cartItem.id);
+        const itemPrice = product.price * cartItem.quantity;
+        cartPrice += itemPrice;
+        cartTotal += itemPrice;
+    });
+
+    setCartPrice(cartPrice, cartDiscount, cartTotal);
+}
+// Add to cart
+
+document.addEventListener("DOMContentLoaded", function () {
+    const addToCartButtons = document.querySelectorAll(".add_to_cart");
+    addToCartButtons.forEach((button) => {
+        button.addEventListener("click", function (e) {
+            e.preventDefault();
+            const productId = e.target.id;
+            addToCart(productId);
+            updatePrice(productId);
+            alert("Product added successfully!");
+        });
+    });
+
+    // Apply Coupon
+    const applyCouponButton = document.getElementById("apply");
+
+    applyCouponButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (cartPrice > 50) {
+            if (!isAppliedCode) {
+                const code = document.getElementById("coupon").value;
+                cartTotal = code == "OSTAD" ? cartTotal - 50 : cartTotal;
+                document.getElementById("cart_total").innerText =
+                    cartTotal.toFixed(2);
+                document.getElementById("cart_discount").innerText = 50;
+                cartDiscount = 50;
+                isAppliedCode = true;
+            }
+        }
+    });
+
+    // Remove Cart Single Item
+    cartContainer.addEventListener("click", function (e) {
+        if (
+            e.target.classList.contains("cart-delete-btn") ||
+            e.target.closest(".cart-delete-btn")
+        ) {
+            const productString = e.target.id;
+            const productId = parseInt(productString.match(/\d+/)[0]);
+
+            // Remove the product from the cartArray
+            cartArray = cartArray.filter((item) => item.id !== productId);
+
+            // Update the cartPrice and cartTotal
+            const productPrice = products.find((p) => p.id === productId).price;
+            cartPrice -= productPrice;
+            cartTotal -= productPrice;
+
+            if (cartPrice < 50) {
+                cartDiscount = 0;
+                cartTotal = cartPrice;
+            }
+
+            // Update the displayed prices
+            setCartPrice(cartPrice, cartDiscount, cartTotal);
+
+            // // Remove the product element from the DOM
+            // productDiv.remove();
+            cartContainer.innerHTML = "";
+            cartArray.forEach((product) => {
+                addCartElement(product);
+            });
+        }
+
+        // Plus Quantity
+        if (e.target.classList.contains("plus-btn")) {
+            const buttonId = e.target.id;
+            const buttonIdNumber = parseInt(buttonId.match(/\d+/)[0]);
+            const quantityElement = document.getElementById(
+                `cart-item-quantity-${buttonIdNumber}`
+            );
+
+            const product = products.find((item) => item.id === buttonIdNumber);
+
+            let currentQuantity = parseInt(quantityElement.textContent);
+            currentQuantity++;
+            quantityElement.textContent = currentQuantity;
+
+            let currentCartItemPrice = document.getElementById(
+                `cart-item-price-${buttonIdNumber}`
+            );
+
+            currentCartItemPrice.textContent = `$${(
+                product.price * currentQuantity
+            ).toFixed(2)}`;
+
+            cartPrice += product.price;
+            cartTotal += product.price;
+            console.log(cartDiscount);
+
+            setCartPrice(cartPrice, cartDiscount, cartTotal);
+        }
+
+        // Minus Quantity
+        if (e.target.classList.contains("minus-btn")) {
+            const buttonId = e.target.id;
+            const buttonIdNumber = parseInt(buttonId.match(/\d+/)[0]);
+            const quantityElement = document.getElementById(
+                `cart-item-quantity-${buttonIdNumber}`
+            );
+
+            const product = products.find((item) => item.id === buttonIdNumber);
+
+            let currentQuantity = parseInt(quantityElement.textContent);
+            if (currentQuantity > 1) {
+                currentQuantity--;
+                quantityElement.textContent = currentQuantity;
+
+                let currentCartItemPrice = document.getElementById(
+                    `cart-item-price-${buttonIdNumber}`
+                );
+
+                currentCartItemPrice.textContent = `$${(
+                    product.price * currentQuantity
+                ).toFixed(2)}`;
+
+                cartPrice -= product.price;
+                cartTotal -= product.price;
+
+                setCartPrice(cartPrice, cartDiscount, cartTotal);
+            }
+        }
+    });
+
+    // Clear Cart
+    const clearCartButton = document.getElementById("remove_all");
+    clearCartButton.addEventListener("click", () => {
+        cartContainer.innerHTML = "";
+        cartPrice = 0;
+        cartDiscount = 0;
+        cartTotal = 0;
+        setCartPrice(cartPrice, cartDiscount, cartTotal);
+    });
+});
+
+function addCartElement(product) {
     const cartItem = document.createElement("div");
     cartItem.classList.add(
         `cart-item-${product.id}`,
@@ -57,18 +217,28 @@ const addToCart = (productId) => {
 
     // Create the minus button
     const minusButton = document.createElement("button");
-    minusButton.classList.add("btn", "btn-sm", "btn-danger", "minus-btn");
-    minusButton.innerHTML = '<i class="fas fa-minus"></i>';
+    minusButton.classList.add(
+        "btn",
+        "btn-sm",
+        "btn-danger",
+        "minus-btn",
+        "px-2",
+        "mx-2"
+    );
+    minusButton.id = `minusBtn-${product.id}`;
+    minusButton.innerHTML = "-";
 
     // Create the quantity text
     const quantityText = document.createElement("span");
     quantityText.classList.add("cart-item-quantity", "fs-", "px-4");
+    quantityText.id = `cart-item-quantity-${product.id}`;
     quantityText.textContent = "1";
 
     // Create the plus button
     const plusButton = document.createElement("button");
-    plusButton.classList.add("btn", "btn-sm", "btn-danger");
-    plusButton.innerHTML = '<i class="fas fa-plus"></i>';
+    plusButton.classList.add("btn", "btn-sm", "btn-danger", "plus-btn");
+    plusButton.innerHTML = "+";
+    plusButton.id = `plusButton-${product.id}`;
 
     // Append the quantity elements to the quantity container
     quantity.append(minusButton, quantityText, plusButton);
@@ -86,6 +256,7 @@ const addToCart = (productId) => {
         "ml-4",
         "pl-2"
     );
+    priceText.id = `cart-item-price-${product.id}`;
     priceText.textContent = `$${product.price}`;
 
     // Create the delete button
@@ -97,8 +268,8 @@ const addToCart = (productId) => {
         "cart-delete-btn",
         "float-right"
     );
+    deleteButton.innerHTML = "Remove";
     deleteButton.id = `delete-btn-${product.id}`;
-    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
 
     // Append the price elements to the price container
     price.append(priceText, deleteButton);
@@ -114,51 +285,11 @@ const addToCart = (productId) => {
 
     // Append the cart item to the cart container
     cartContainer.appendChild(cartItem);
-};
+}
 
-function updatePrice(productId) {
-    const product = products.find((p) => p.id == productId);
-    cartPrice += product.price;
-    cartTotal += product.price;
-
+function setCartPrice(cartPrice, cartDiscount, cartTotal) {
     document.getElementById("cart_price").innerText = cartPrice.toFixed(2);
     document.getElementById("cart_total").innerText = cartTotal.toFixed(2);
+    document.getElementById("cart_discount").innerHTML =
+        cartDiscount.toFixed(2);
 }
-// Add to cart
-
-document.addEventListener("DOMContentLoaded", function () {
-    const addToCartButtons = document.querySelectorAll(".add_to_cart");
-    addToCartButtons.forEach((button) => {
-        button.addEventListener("click", function (e) {
-            e.preventDefault();
-            const productId = e.target.id;
-            addToCart(productId);
-            updatePrice(productId);
-            alert("Product added successfully!");
-        });
-    });
-
-    // Apply Coupne
-    const applyCouponButton = document.getElementById("apply");
-
-    applyCouponButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        if (cartPrice > 50) {
-            const code = document.getElementById("coupon").value;
-            cartTotal = code == "OSTAD" ? cartTotal - 50 : cartTotal;
-            document.getElementById("cart_total").innerText =
-                cartTotal.toFixed(2);
-            document.getElementById("cart_discount").innerText = 50;
-        }
-    });
-
-    // Remove Cart Single Item
-    cartContainer.addEventListener("click", function (e) {
-        if (
-            e.target.classList.contains("cart-delete-btn") ||
-            e.target.closest(".cart-delete-btn")
-        ) {
-            console.log("OK");
-        }
-    });
-});
